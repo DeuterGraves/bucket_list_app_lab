6b2c9b19d7fe63afbaeed531a33c00dc5499b663
DeuterGraves <puckles@gmail.com>
2018-10-31 13:40:06 +0000

    all bucklist item details rendering

--- client/src/views/bucket_list_item.js
+++ client/src/views/bucket_list_item.js
@@ -11,9 +11,49 @@ BucketListItemView.prototype.render = function (bucketListItem) {
 
   const title = this.createHeading(bucketListItem.title);
   // append item created with create heading to the container
-  bucketListItemContainer.appendChild(title);
 
-console.log("render called", bucketListItem.title);
+// console.log("render called", bucketListItem.title);
+
+//  description
+  const description = this.createTextElement("p", bucketListItem.description)
+
+// location - not all
+const location = document.createElement("p");
+if (!bucketListItem.location){
+  location.textContent = ""
+}else{
+  // call createTextElement if there is content - pass elementType and text
+  location.textContent = `Location: ${bucketListItem.location} `
+}
+//  deadline - not all
+const deadline = document.createElement("p");
+if (!bucketListItem.deadline){
+  deadline.textContent = ""
+}else{
+  // call createTextElement if there is content - pass elementType and text
+  deadline.textContent = `Deadline: ${bucketListItem.deadline} `
+}
+
+// image - not all
+const image = document.createElement("img");
+if (!bucketListItem.image){
+  image.textContent = ""
+}else{
+  // call createTextElement if there is content - pass elementType and text
+  image.setAttribute("src", bucketListItem.image)
+  image.setAttribute("alt", `${bucketListItem.title} `)
+  image.setAttribute("width", "200")
+}
+
+
+const lineBreak = document.createElement("hr")
+
+bucketListItemContainer.appendChild(title);
+bucketListItemContainer.appendChild(description)
+bucketListItemContainer.appendChild(location)
+bucketListItemContainer.appendChild(deadline)
+bucketListItemContainer.appendChild(image)
+bucketListItemContainer.appendChild(lineBreak)
 
 // append this conatiner to the document
 this.container.appendChild(bucketListItemContainer)
@@ -23,9 +63,14 @@ this.container.appendChild(bucketListItemContainer)
 BucketListItemView.prototype.createHeading= function (textContent) {
   const heading = document.createElement('h3');
   heading.textContent = textContent;
+  // console.log("create heading called");
   return heading;
+};
 
-  console.log("create heading called");
+BucketListItemView.prototype.createTextElement = function (elementType, text) {
+  const element = document.createElement(elementType);
+  element.textContent = text;
+  return element;
 
 };
 
